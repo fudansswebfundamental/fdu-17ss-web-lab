b59cda39cec41fb1c3b318489e7073c84d0d6247
Currycurrycurry <17302010063@fudan.edu.cn>
2018-05-19 20:05:23 +0800

    lab07

--- functions.inc.php
+++ functions.inc.php
@@ -1,15 +1,62 @@
 <?php
 
 function generateLink($url, $label, $class) {
-   $link = '<a href="' . $url . '" class="' . $class . '">';
-   $link .= $label;
-   $link .= '</a>';
-   return $link;
+    $link = '<a href="' . $url . '" class="' . $class . '">';
+    $link .= $label;
+    $link .= '</a>';
+    return $link;
 }
-
 
 function outputPostRow($number)  {
     include("travel-data.inc.php");
+    echo "<div class=\"row\">
+<div class=\"col-md-4\">".
+        generateLink("post.php?id={${postId.$number}}","<img src=images/{${thumb.$number}} alt={${title.$number}} class=img-responsive/>","")
+        .
+        "
+    </div>
+    <div class=\"col-md-8\">
+    <h2>{${title.$number}}</h2>
+    <div class=\"details\">
+    Posted by
+    "
+        .
+        generateLink("user.php?id={${userId.$number}}","{${userName.$number}}","")
+        .
+        "
+    <span class=\"pull-right\">{${date.$number}}</span>
+   <p class=\"ratings\">"
+        .
+        constructRating(${reviewsRating.$number})
+        .
+        "
+   {${reviewsNum.$number}} Reviews
+    </p>
+    </div>
+    <p class=\"excerpt\">{${excerpt.$number}}</p>
+    <p>
+    "
+        .
+        generateLink("post.php?id={${postId.$number}}","Read more","btn btn-primary btn-sm")
+        .
+        "
+    </p>
+    </div>
+    </div>
+    <hr/>
+        ";
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 /*
@@ -18,18 +65,18 @@ function outputPostRow($number)  {
 */
 function constructRating($rating) {
     $imgTags = "";
-    
+
     // first output the gold stars
     for ($i=0; $i < $rating; $i++) {
         $imgTags .= '<img src="images/star-gold.svg" width="16" />';
     }
-    
+
     // then fill remainder with white stars
     for ($i=$rating; $i < 5; $i++) {
         $imgTags .= '<img src="images/star-white.svg" width="16" />';
-    }    
-    
-    return $imgTags;    
+    }
+
+    return $imgTags;
 }
 
 ?>
