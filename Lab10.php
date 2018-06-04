cd45110710cd280a55e7ac6a71e6e6b3fb15b880
zhsyy <17302010053>
2018-06-04 23:11:30 +0800

    Lab10

--- Lab10.php
+++ Lab10.php
@@ -3,7 +3,12 @@
 
 //****** Hint ******
 //connect database and fetch data here
+//
+$servername = "localhost";
+$username = "root";
+$password = "";
 
+$conn = new mysqli($servername, $username, $password, travel);
 
 ?>
 
@@ -46,7 +51,8 @@
 
                 //****** Hint ******
                 //display the list of continents
-
+                $sql = "SELECT ContinentCode, ContinentName FROM continents";
+                $result = $conn->query($sql);
                 while($row = $result->fetch_assoc()) {
                   echo '<option value=' . $row['ContinentCode'] . '>' . $row['ContinentName'] . '</option>';
                 }
@@ -56,11 +62,17 @@
               
               <select name="country" class="form-control">
                 <option value="0">Select Country</option>
+
                 <?php 
                 //Fill this place
 
                 //****** Hint ******
-                /* display list of countries */ 
+                /* display list of countries */
+                $sql = "SELECT CountryName, ISO FROM countries";
+                $result = $conn->query($sql);
+                while($row = $result->fetch_assoc()) {
+                    echo '<option value=' . $row['ISO'] . '>' . $row['CountryName'] . '</option>';
+                }
                 ?>
               </select>    
               <input type="text"  placeholder="Search title" class="form-control" name=title>
@@ -73,7 +85,8 @@
                                     
 
 		<ul class="caption-style-2">
-            <?php 
+            <?php
+
             //Fill this place
 
             //****** Hint ******
@@ -89,7 +102,58 @@
                 </div>
               </a>
             </li>        
-            */ 
+            */
+            $Continent = $_GET['continent'];
+            $Country = $_GET['country'];
+
+            if($Continent !== 0) {
+                $result = mysqli_query($conn, "SELECT * FROM imagedetails WHERE ContinentCode='$Continent'");
+                while($row = mysqli_fetch_array($result)) {
+                    echo "<li>
+              <a href=\"detail.php?id=".$row['ImageID']."\" class=\"img-responsive\">
+                <img src=\"images/square-medium/".$row['Path']."\" alt=\"".$row['Title']."\">
+                <div class=\"caption\">
+                  <div class=\"blur\"></div>
+                  <div class=\"caption-text\">
+                    <p>".$row['Description']."</p>
+                  </div>
+                </div>
+              </a>
+            </li>";
+                }
+            }
+            if($Country !== 0){
+                $result = mysqli_query($conn, "SELECT * FROM imagedetails WHERE CountryCodeISO='$Country'");
+                while($row = mysqli_fetch_array($result)) {
+                    echo "<li>
+              <a href=\"detail.php?id=".$row['ImageID']."\" class=\"img-responsive\">
+                <img src=\"images/square-medium/".$row['Path']."\" alt=\"".$row['Title']."\">
+                <div class=\"caption\">
+                  <div class=\"blur\"></div>
+                  <div class=\"caption-text\">
+                    <p>".$row['Description']."</p>
+                  </div>
+                </div>
+              </a>
+            </li>";
+                }
+            }
+            if($Country == "0" && $Continent == "0"){
+                $result = mysqli_query($conn, "SELECT * FROM imagedetails WHERE ImageID!='0'");
+                while($row = mysqli_fetch_array($result)) {
+                    echo "<li>
+              <a href=\"detail.php?id=".$row['ImageID']."\" class=\"img-responsive\">
+                <img src=\"images/square-medium/".$row['Path']."\" alt=\"".$row['Title']."\">
+                <div class=\"caption\">
+                  <div class=\"blur\"></div>
+                  <div class=\"caption-text\">
+                    <p>".$row['Description']."</p>
+                  </div>
+                </div>
+              </a>
+            </li>";
+                }
+            }
             ?>
        </ul>       
 
